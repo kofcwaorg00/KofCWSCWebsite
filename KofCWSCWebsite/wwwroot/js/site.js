5430353b86f9913b0be6ae6787944cc60f7b4ceb
tphilomeno <tphilomeno@comcast.net>
2024-09-26 13:15:09 -0400

    add spinner to pdf viewer and move state directory to members menu

--- KofCWSCWebsite/wwwroot/js/site.js
+++ KofCWSCWebsite/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function displayBusyIndicator() {
+    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingtext").style.display = "block";
+}
+function hideBusyIndicator() {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("loadingtext").style.display = "none";
+}
