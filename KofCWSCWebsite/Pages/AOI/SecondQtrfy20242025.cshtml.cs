af026f086399e4f51f6e196f03c01766fc4e5d74
Dang H. Nguyen <haidangwa@gmail.com>
2024-10-06 13:55:07 -0700

    adds Second Qtr FY 2024-2025 Membership Resources AOI

---
+++ KofCWSCWebsite/Pages/AOI/SecondQtrfy20242025.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KofCWSCWebsite.Pages.AOI
+{
+    public class SecondQtrfy20242025Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
