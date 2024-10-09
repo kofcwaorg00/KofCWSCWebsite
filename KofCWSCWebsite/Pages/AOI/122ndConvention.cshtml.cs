a28441ab050161758f2740d3b6fa7cd74585a016
Dang H. Nguyen <haidangwa@gmail.com>
2024-10-09 12:59:32 -0700

    adds a page for the 122nd State Convention

---
+++ KofCWSCWebsite/Pages/AOI/122ndConvention.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KofCWSCWebsite.Pages.AOI
+{
+    public class 122ndConventionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
