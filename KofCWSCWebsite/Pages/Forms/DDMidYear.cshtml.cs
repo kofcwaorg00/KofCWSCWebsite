dbbb614f37984116a83279ab4e106968fe71bb97
tphilomeno <tphilomeno@comcast.net>
2024-10-30 10:03:22 -0400

    add popup modal for state directory and ddmidyear meeting

---
+++ KofCWSCWebsite/Pages/Forms/DDMidYear.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KofCWSCWebsite.Pages.Forms
+{
+    public class DDMidYearModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
