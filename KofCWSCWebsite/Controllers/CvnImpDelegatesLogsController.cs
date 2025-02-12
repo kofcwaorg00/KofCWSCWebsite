c8129ee48d8b236194389db095fc3a5e511e4cd1
Tim Philomeno <tphilomeno@kofc-wa.org>
2025-02-12 15:13:56 -0500

    add cvnimpdelegateslog view and filter

--- KofCWSCWebsite/Controllers/CvnImpDelegatesLogsController.cs
+++ KofCWSCWebsite/Controllers/CvnImpDelegatesLogsController.cs
@@ -20,12 +20,12 @@ namespace KofCWSCWebsite.Controllers
     //---------------------------------------------------------------------------------------
     public class CvnImpDelegatesLogsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApiHelper _apiHelper;
         private KofCWSCWebsite.Data. DataSetService _dataSetService;
 
-        public CvnImpDelegatesLogsController(ApplicationDbContext context, DataSetService dataSetService)
+        public CvnImpDelegatesLogsController(DataSetService dataSetService,ApiHelper apiHelper)
         {
-            _context = context;
+            _apiHelper = apiHelper;
             _dataSetService = dataSetService;
         }
 
@@ -33,161 +33,163 @@ namespace KofCWSCWebsite.Controllers
         [Authorize(Roles = "Admin, ConventionAdmin")]
         public async Task<IActionResult> Index()
         {
-            return View(await _context.TblCvnImpDelegatesLogs.ToListAsync());
+            var results = await _apiHelper.GetAsync<IEnumerable<CvnImpDelegatesLog>>("ImpDelegateLogs");
+
+            return View(results.OrderBy(o => o.Rundate));
         }
 
-        // GET: CvnImpDelegatesLogs/Details/5
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CvnImpDelegatesLogs/Details/5
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cvnImpDelegatesLog == null)
-            {
-                return NotFound();
-            }
+        //    var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (cvnImpDelegatesLog == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(cvnImpDelegatesLog);
-        }
+        //    return View(cvnImpDelegatesLog);
+        //}
 
-        // GET: CvnImpDelegatesLogs/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //// GET: CvnImpDelegatesLogs/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: CvnImpDelegatesLogs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> Create([Bind("Id,Guid,Rundate,Type,MemberId,Data")] CvnImpDelegatesLog cvnImpDelegatesLog)
-        {
-            if (ModelState.IsValid)
-            {
-                Uri myURI = new(_dataSetService.GetAPIBaseAddress() + "/CreateLogEntry");
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = myURI;
-                    var response = await client.PostAsJsonAsync(myURI, cvnImpDelegatesLog);
-                    try
-                    {
-                        response.EnsureSuccessStatusCode();
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Error(ex.Message + ' ' + ex.InnerException);
-                    }
-                }
-            }
+        //// POST: CvnImpDelegatesLogs/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> Create([Bind("Id,Guid,Rundate,Type,MemberId,Data")] CvnImpDelegatesLog cvnImpDelegatesLog)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Uri myURI = new(_dataSetService.GetAPIBaseAddress() + "/CreateLogEntry");
+        //        using (var client = new HttpClient())
+        //        {
+        //            client.BaseAddress = myURI;
+        //            var response = await client.PostAsJsonAsync(myURI, cvnImpDelegatesLog);
+        //            try
+        //            {
+        //                response.EnsureSuccessStatusCode();
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                Log.Error(ex.Message + ' ' + ex.InnerException);
+        //            }
+        //        }
+        //    }
 
 
-            if (ModelState.IsValid)
-            {
-                _context.Add(cvnImpDelegatesLog);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cvnImpDelegatesLog);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(cvnImpDelegatesLog);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(cvnImpDelegatesLog);
+        //}
 
-        // GET: CvnImpDelegatesLogs/Edit/5
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CvnImpDelegatesLogs/Edit/5
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs.FindAsync(id);
-            if (cvnImpDelegatesLog == null)
-            {
-                return NotFound();
-            }
-            return View(cvnImpDelegatesLog);
-        }
+        //    var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs.FindAsync(id);
+        //    if (cvnImpDelegatesLog == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(cvnImpDelegatesLog);
+        //}
 
-        // POST: CvnImpDelegatesLogs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Guid,Rundate,Type,MemberId,Data")] CvnImpDelegatesLog cvnImpDelegatesLog)
-        {
-            if (id != cvnImpDelegatesLog.Id)
-            {
-                return NotFound();
-            }
+        //// POST: CvnImpDelegatesLogs/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Guid,Rundate,Type,MemberId,Data")] CvnImpDelegatesLog cvnImpDelegatesLog)
+        //{
+        //    if (id != cvnImpDelegatesLog.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cvnImpDelegatesLog);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CvnImpDelegatesLogExists(cvnImpDelegatesLog.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cvnImpDelegatesLog);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(cvnImpDelegatesLog);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!CvnImpDelegatesLogExists(cvnImpDelegatesLog.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(cvnImpDelegatesLog);
+        //}
 
-        // GET: CvnImpDelegatesLogs/Delete/5
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: CvnImpDelegatesLogs/Delete/5
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (cvnImpDelegatesLog == null)
-            {
-                return NotFound();
-            }
+        //    var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (cvnImpDelegatesLog == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(cvnImpDelegatesLog);
-        }
+        //    return View(cvnImpDelegatesLog);
+        //}
 
-        // POST: CvnImpDelegatesLogs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Admin, ConventionAdmin")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs.FindAsync(id);
-            if (cvnImpDelegatesLog != null)
-            {
-                _context.TblCvnImpDelegatesLogs.Remove(cvnImpDelegatesLog);
-            }
+        //// POST: CvnImpDelegatesLogs/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin, ConventionAdmin")]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var cvnImpDelegatesLog = await _context.TblCvnImpDelegatesLogs.FindAsync(id);
+        //    if (cvnImpDelegatesLog != null)
+        //    {
+        //        _context.TblCvnImpDelegatesLogs.Remove(cvnImpDelegatesLog);
+        //    }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
-        private bool CvnImpDelegatesLogExists(int id)
-        {
-            return _context.TblCvnImpDelegatesLogs.Any(e => e.Id == id);
-        }
+        //private bool CvnImpDelegatesLogExists(int id)
+        //{
+        //    return _context.TblCvnImpDelegatesLogs.Any(e => e.Id == id);
+        //}
     }
 }
