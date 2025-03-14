b277bdddcd6d6a32af850b34cdf782f9cbde1e90
Tim Philomeno <tphilomeno@kofc-wa.org>
2025-03-14 10:39:12 -0400

    continue to adjust import formatting

--- KofCWSCWebsite/Models/CvnImpDelegate.cs
+++ KofCWSCWebsite/Models/CvnImpDelegate.cs
@@ -18,20 +18,29 @@ public partial class CvnImpDelegate
 
     [Display(Name = "Council #")]
     public int? CouncilNumber { get; set; }
-
+    [Display(Name = "Imported First Name")]
     public string? D1FirstName { get; set; }
+    [Display(Name = "Imported Middle Name")]
     public string? D1MiddleName { get; set; }
+    [Display(Name = "Imported Last Name")]
     public string? D1LastName { get; set; }
+    [Display(Name = "Imported Suffix")]
     public string? D1Suffix { get; set; }
+    [Display(Name = "Imported KofC ID")]
     public int? D1MemberID { get; set; }
+    [Display(Name = "Imported Address")]
     public string? D1Address1 { get; set; }
-
+    [Display(Name = "Imported Address 2")]
     public string? D1Address2 { get; set; }
+    [Display(Name = "Imported City")]
     public string? D1City { get; set; }
+    [Display(Name = "Imported State")]
     public string? D1State { get; set; }
+    [Display(Name = "Imported Zip")]
     public string? D1ZipCode { get; set; }
+    [Display(Name = "Imported Phone")]
     public string? D1Phone { get; set; }
-
+    [Display(Name = "Imported Email")]
     public string? D1Email { get; set; }
 
     public string? D2FirstName { get; set; }
@@ -111,27 +120,29 @@ public partial class CvnImpDelegate
     public string? Validation {  get; set; }
     public string? RecType { get; set; }
 
+    [Display(Name = "Existing First Name")]
     public string? ED1FirstName { get; set; }
+    [Display(Name = "Existing Middle Name")]
     public string? ED1MiddleName { get; set; }
-
+    [Display(Name = "Existing Last Name")]
     public string? ED1LastName { get; set; }
-
+    [Display(Name = "Existing Suffix")]
     public string? ED1Suffix { get; set; }
-    
+    [Display(Name = "Existing KofC ID")]
     public int? ED1MemberID { get; set; }
-
+    [Display(Name = "Existing Address")]
     public string? ED1Address1 { get; set; }
-
+    [Display(Name = "Existing Address 2")]
     public string? ED1Address2 { get; set; }
-
+    [Display(Name = "Existing City")]
     public string? ED1City { get; set; }
-
+    [Display(Name = "Existing State")]
     public string? ED1State { get; set; }
-
+    [Display(Name = "Existing Zip")]
     public string? ED1ZipCode { get; set; }
-    
+    [Display(Name = "Existing Phone")]
     public string? ED1Phone { get; set; }
-    
+    [Display(Name = "Existing Email")]
     public string? ED1Email { get; set; }
 
     public string? ED2FirstName { get; set; }
