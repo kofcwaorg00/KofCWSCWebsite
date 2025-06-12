6bef39c5611470b6ef64c9440af6b5d9c59d9ccd
Tim Philomeno <tphilomeno@kofc-wa.org>
2025-06-12 14:00:44 -0400

    addded Bulletin URL

---
+++ KofCWSCWebsite/Pages/Bulletin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KofCWSCWebsite.Pages
+{
+    public class BulletinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
