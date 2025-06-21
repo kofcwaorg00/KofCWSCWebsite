a86a41181f1e71947a977c177f7f5884052d8cb0
Tim Philomeno <tphilomeno@kofc-wa.org>
2025-06-21 09:44:39 -0400

    add update all to logcorrmemberoffice

---
+++ KofCWSCWebsite/Database/Sprocs/dbo.uspLOG_UpdateProcessFlag.sql
@@ -0,0 +1,18 @@
+﻿alter PROCEDURE uspLOG_UpdateProcessFlag
+@ID INT
+AS
+BEGIN
+--select * from tblLOG_CorrMemberOffice
+    IF(@ID = 0) -- if we pass in 0 do them all
+    BEGIN
+        UPDATE tblLOG_CorrMemberOffice
+    SET    Processed = 1
+    END
+    ELSE
+    BEGIN
+        UPDATE tblLOG_CorrMemberOffice
+    SET    Processed = 1
+    WHERE  ID = @ID;
+    END
+
+END
