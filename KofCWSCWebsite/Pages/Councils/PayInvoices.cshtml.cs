7c94be4f0728924155fa3f426c93787f6d6c651d
Dang Nguyen <haidangwa@gmail.com>
2025-07-29 20:49:32 -0700

    adds a "Pay Invoices" page and link in the Members Only menu

---
+++ KofCWSCWebsite/Pages/Councils/PayInvoices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KofCWSCWebsite.Pages.Councils
+{
+    public class PayInvoices : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
